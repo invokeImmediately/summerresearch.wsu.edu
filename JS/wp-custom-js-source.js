c3415aa1a32977bd91e5dac835565547a74a46aa
Daniel Rieck <danielcrieck@gmail.com>
2016-06-24 23:20:48 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code.

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -76,6 +76,7 @@
 	
 	function fillHiddenFields(fieldsToFill) {
 		if(fieldsToFill instanceof FieldsToFill && fieldsToFill.isValid()) {
+			alert("Guard passed.");
 			switch(fieldsToFill.selectionMade) {
 				case "Ali Mehrizi-Sani (USPRISM: U.S.-Scotland Program for Research on Integration of Renewable Energy Resources and SMart Grid)":
 					fieldsToFill.$emailInputBox.val("mehrizi@eecs.wsu.edu");
@@ -98,6 +99,7 @@
 					fieldsToFill.$nameInputBox.val("Gretchen");
 					break;
 				case "Larry Holder (Smart Environments)":
+					alert("Switch passed.");
 					fieldsToFill.$emailInputBox.val("holder@wsu.edu");
 					fieldsToFill.$nameInputBox.val("Larry");
 					break;
