e0f1d9d5d43221806dd81e8a8e494860c5af02e3
Daniel Rieck <danielcrieck@gmail.com>
2016-08-01 11:18:08 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code.

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -332,15 +332,26 @@ function isJQuery($obj) {
         var scalingAmount = scalingFactor || 1,
             settings = $.extend({
                 "minFontSize" : Number.NEGATIVE_INFINITY,
-                "maxFontSize" : Number.POSITIVE_INFINITY
+                "maxFontSize" : Number.POSITIVE_INFINITY,
+				"againstSelf" : true
             }, options);
         return this.each(function () {
             var $this = $(this);
+			var $parent = undefined;
+			if(settings.measuredBy = "parent") {
+				$parent = $this.parents("div.column").first();
+			}
           
             // Resizer() keeps font-size proportional to object width as constrainted by the user
             var resizer = function () {
-                $this.css("font-size", Math.max(Math.min($this.width() / (scalingAmount*10),
-                    parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+				if(!settings.againstSelf) {
+					$this.css("font-size", Math.max(Math.min($parent.innerWidth() / (scalingAmount*10),
+						parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+				}
+				else {
+					$this.css("font-size", Math.max(Math.min($this.width() / (scalingAmount*10),
+						parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+				}
             };
           
             // Call once to set the object's font size based on current window size, then call as
@@ -349,7 +360,41 @@ function isJQuery($obj) {
             $(window).on("resize.textresize orientationchange.textresize", resizer);
         });
     };
-    
+	
+// TODO: write function for fitting text.
+//	$.fn.fitText = function(  )
+
+// TODO: Come up with a line-based solution
+//  Ideas: invisible absolutely positioned duplicate of element that is scaled until desired effect is
+//   achieved, then settings are applied to original; etc.
+/*	function FontShrinker($fromElem) {
+		this.maxLines = undefined;
+		this.leadingRatio = undefined;
+		this.fontSizeStart = undefined;
+		this.fontSizeThreshold = undefined;
+		
+		var validArg = isJQuery($fromElem);
+		if(validArg) {
+			this.maxLines = $this.data("max-lines");
+			var styleProps = $this.css([
+				"fontSize", "lineHeight"
+			]);
+			styleProps = $.extend({
+				"height" : $this.height()
+			}, styleProps);
+			var height = parseFloat(styleProps.height);
+			var fontSize = parseFloat(styleProps.fontSize);
+			var lineHeight = parseFloat(styleProps.lineHeight);
+			this.leadingRatio = parseFloat(styleProps.lineHeight) / parseFloat(styleProps.fontSize);
+			var curLines = height / lineHeight;
+			if(this.maxLines != undefined && curLines > maxLines) {
+				var newFontSz = 
+			} else {
+				
+			}
+		}
+	}*/
+
     // Now use the plugin on the WSU Undergraduate education website (i.e. delete or modify the
     // following statement if you are going to utilize this plugin on your own site).
     $(document).ready(function () {
@@ -366,12 +411,34 @@ function isJQuery($obj) {
 		var $fittedElems = $(".auto-fits-text");
 		$fittedElems.each(function() {
 			var $this = $(this);
+			var $parent = $this.parents("div.column").first();
 			var fontSz = $this.css("font-size");
-			var scalingAmt = clmnWidth / (fontSz * 10);
-			$this.textResize(scalingAmt, {"minFontSize" : "10.7"})
+			var maxWidth = $parent.css("max-width");
+			var scalingAmt;
+			if (maxWidth == "none") {
+				var $binder = $("#binder");
+				if ($binder.length == 1) {
+					maxWidth = $binder.css("max-width");
+					if (maxWidth != "none") {
+						clmnWidth = parseFloat(maxWidth) - 198;
+					}
+				}
+				scalingAmt = clmnWidth / (parseFloat(fontSz) * 10);
+			}
+			else {
+				scalingAmt = parseFloat(maxWidth) / (parseFloat(fontSz) * 10);
+			}
+			$this.textResize(scalingAmt, {"minFontSize" : "10.7px", "againstSelf" : 0})
 		});
+		
+/*		var $shrinkingElems = $(".shrinks-with-parent");
+		$shrinkingElems.each(function() {
+			var $this = $(this);
+			
+		});*/
     });
 })(jQuery);
+// 14.4px;
 /**************************************************************************************************\
 | JQUERY-MEDIATED ENHANCED INTERACTIVITY OF GRAVITY FORM FIELDS                                    |
 \**************************************************************************************************/
