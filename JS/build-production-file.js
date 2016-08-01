8c266194fe2c7d29e198e626867b12dd76f05254
Daniel Rieck <danielcrieck@gmail.com>
2016-08-01 11:14:05 -0700

    Adding new dependency to JS build order
    Now utilizing the jQuery.textResize.js plugin.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -3,6 +3,7 @@ var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files'
 concat([
  '../WSU-UE---JS/jQuery.oue-custom.js',
  './sumres-custom.js',
+ '../WSU-UE---JS/jQuery.textResize.js',
  '../WSU-UE---JS/jQuery.forms.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.autoScrollingImages.js',
