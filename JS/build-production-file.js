eb30c4c30cc2cd36e520bbd2e562135bd3612d76
Daniel Rieck <danielcrieck@gmail.com>
2017-06-02 12:04:21 -0700

    Updating build order for custom JS
    Masonry JS and imagesloaded packages now being imported from cloned GitHub repositories, vs. as previously wherein they were embedded in custom JS file.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -6,7 +6,9 @@ concat([
  '../WSU-UE---JS/jQuery.forms.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.autoScrollingImages.js',
- '../WSU-UE---JS/jQuery.masonry.min.js',
+ '../../imagesloaded/imagesloaded.pkgd.min.js',
+ '../../masonry/dist/masonry.pkgd.min.js',
+ '../WSU-UE---JS/jQuery.masonry-custom.js',
  './sumres-custom.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
