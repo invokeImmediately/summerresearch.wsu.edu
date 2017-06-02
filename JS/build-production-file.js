019a59c4f1c4fc290135a1a18b2df9677b823778
Daniel Rieck <danielcrieck@gmail.com>
2017-06-02 10:15:44 -0700

    Chaning inclusion order of build dependencies
    Masonry JS needs to be loaded before the site-specific custom JS file is included due to it being a dependency in planned features now being implemented.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -2,12 +2,12 @@
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
  '../WSU-UE---JS/jQuery.oue-custom.js',
- './sumres-custom.js',
  '../WSU-UE---JS/jQuery.textResize.js',
  '../WSU-UE---JS/jQuery.forms.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.autoScrollingImages.js',
- '../WSU-UE---JS/jQuery.masonry.min.js'
+ '../WSU-UE---JS/jQuery.masonry.min.js',
+ './sumres-custom.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
  });
