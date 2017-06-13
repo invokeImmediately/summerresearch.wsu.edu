a2ae821544fa813c71b64b8b63f2f793ff7d1988
Daniel Rieck <danielcrieck@gmail.com>
2017-06-13 10:45:37 -0700

    Documenting code, debugging semnatic error
    Documenting scrolling adjustment functions for ensuring anchors are visible after they have been navigated to, fixing an error involving an unnecessary function argument

--- JS/sumres-custom.js
+++ JS/sumres-custom.js
@@ -1,61 +1,115 @@
 // See [https://github.com/invokeImmediately/distinguishedscholarships.wsu.edu] for repository of source code
-/************************************************************************************************************
+/*!**********************************************************************************************************
  * CUSTOM JQUERY-BASED DYNAMIC CONTENT                                                                      *
  ************************************************************************************************************/
 (function ($) {
+
+/**
+ * IIFE for wrapping statements to be executed once the DOM is ready 
+ */
 $(function () {
-	/**********************************************************************************************
-	 * Tweak HTML source to work around some quirks of WordPress setup                            *
-	 **********************************************************************************************/
+
+	// Tweak HTML source to work around some quirks of WordPress setup                            *
 	var siteURL = window.location.pathname;
 	switch (siteURL) {
 		/* case '/':
-		$('#menu-item-35').remove();
-		$('#spine-sitenav ul li').first().css('border-top', 'none');
-		$('#spine-sitenav').addClass('homeless');
-		break;*/
-	case '/news/':
-		$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-		break;
+			$('#menu-item-35').remove();
+			$('#spine-sitenav ul li').first().css('border-top', 'none');
+			$('#spine-sitenav').addClass('homeless');
+			break;*/
+		case '/news/':
+			$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
+			break;
 	}
-	initAnchorFix("#wpadminbar", ".vpue-jump-bar");
+
+	initAnchorFix();
 	initDelayedNotices("p.notice", "is-delayed");
 	initExpiringItems(".has-expiration", "expirationDate", "is-expired");
 	initFacultyEmailAutoEntry("li.gfield.sets-faculty-email", "li.gform_hidden");
 });
 
-function initAnchorFix(slctrToc) {
-	var $toc = $(slctrToc);
-	if($toc.length) {
-		window.onhashchange = adjustScrollingAfterAnchor;
-	}
+// Bind a function to the hashchange event for applying corrections to scrolling
+// position after an anchor has been navigated to. This is necessary because on
+// OUE websites built using the WSU Spine framework, there are instances where
+// elements end up covering the anchor at the top of the screen.
+function initAnchorFix() {
+	window.onhashchange = adjustScrollingAfterNavToAnchor;
 }
 
-function adjustScrollingAfterAnchor() {
-	var currentScrollPos = ($(window).scrollTop() || $("body").scrollTop());
-	var windowWidth = $(window).width();
-	var scrollingAdjustment = 0;
-	var $wpadminbar = $("#wpadminbar");
-	var $spineHeader = $("#spine").find(".spine-header");
-	var $toc = $(".vpue-jump-bar").first();
-	var tocTrigger = $toc.offset().top + $toc.height() + 100;
-	var $floatingToc = $(".vpue-jump-bar.floating");
-	var updatedScrollPos;
-	if ($wpadminbar.length && $wpadminbar.css("top") === "0px") {
+// Function bound to the hashchange event that applying corrections to scrolling
+// position after an anchor has been navigated to. Compensates for instances
+// where elements end up covering the anchor at the top of the screen.
+function adjustScrollingAfterNavToAnchor() {
+	var currentScrollPos;	// Uncorrected scrolling position that resulted
+							// after navigating to the anchor.
+
+	var windowWidth;	// Browser window width is checked against spine header
+						// width to test for mobile/tablet mode.
+
+	var scrollingAdjustment;	// Calculated number of pixels needed to move up
+								// the page and bring the anchor back into view.
+
+	var $wpadminbar;	// jQuery object: WordPress admin bar that floats at top
+						// or bottom of screen.
+
+	var $spineHeader;	// jQuery object: spine header element that floats at
+						// top of screen on mobile/tablet.
+
+	var $toc;	// jQuery object: fixed TOC element located toward top of page.
+
+	var tocTrigger;	// Scrolling position where, once reached, the floating
+					// TOC is brought into view.
+
+	var $floatingToc;	// jQuery object: floating TOC element.
+
+	var updatedScrollPos;		// Corrected scrolling position whereat the
+								// anchor is visible at top of page.
+
+	// Get the current, uncorrected scrolling position 
+	currentScrollPos = ( $(window).scrollTop() || $( "body" ).scrollTop() );
+	scrollingAdjustment = 0;
+
+	// If necessary, compensate for the floating WordPress admin bar
+	$wpadminbar = $( "#wpadminbar" );
+	if ( $wpadminbar.length && $wpadminbar.css( "top" ) === "0px" ) {
 		scrollingAdjustment += $wpadminbar.outerHeight();
 	}
-	if($spineHeader.width() != windowWidth) {
-		if ($floatingToc.length && currentScrollPos > tocTrigger) {
+
+	// If necessary, compensate for the floating spine menu and the OUE-specific
+	// floating TOC feature
+	windowWidth = $( window ).width();
+	$spineHeader = $( "#spine" ).find(".spine-header");
+	$toc = $( ".vpue-jump-bar" ).first();
+	tocTrigger = $toc.offset().top + $toc.height() + 100;
+	$floatingToc = $( ".vpue-jump-bar.floating" );
+	if( $spineHeader.width() != windowWidth ) {
+
+		// We are in desktop view and the spine is positioned to the left of the
+		// main content area. The only possible correction that needs to be
+		// applied on OUE websites is to compensate for a floating TOC element
+		// that is hovering over main content at the top of the screen.
+		if ( $floatingToc.length && currentScrollPos > tocTrigger ) {
 			scrollingAdjustment += $floatingToc.outerHeight() + 8;
 		}
 	} else {
+
+		// We are in mobile/tablet view and the spine is floating at the top of
+		// the screen & covering main content area. It is also possible that a
+		// correction needs to be applied for a floating TOC element that is
+		// peeking out from under the spine header and is also covering main
+		// content.
 		scrollingAdjustment += $spineHeader.outerHeight();
-		if ($floatingToc.length && currentScrollPos > tocTrigger) {
+		if ( $floatingToc.length && currentScrollPos > tocTrigger ) {
 			scrollingAdjustment += 23;
 		}				
 	}
-	updatedScrollPos = currentScrollPos >= scrollingAdjustment ? currentScrollPos - scrollingAdjustment : 0;
-	$("html, body").scrollTop(updatedScrollPos);
+
+	// Correct the scrolling position to bring the anchored element back into
+	// view at visible top of screen
+	updatedScrollPos = currentScrollPos >= scrollingAdjustment ?
+		currentScrollPos - scrollingAdjustment :
+		0;
+	$("html, body").scrollTop( updatedScrollPos );
 }
 
 function initDelayedNotices(slctrNotices, clssIsDelayed) {
@@ -79,6 +133,7 @@ function initExpiringItems(slctrExpiringElems, dataAttrExprtnDate, clssExpired) 
 		$this = $(this);
 		exprtnDateVal = $this.data(dataAttrExprtnDate);
 		if (exprtnDateVal != undefined) {
+
 			// TODO: use regex to enforce correct date format strings
 			exprtnDateObj = new Date(exprtnDateVal);
 			if (today > exprtnDateObj) {
@@ -122,6 +177,7 @@ function resortListsWithExpiredItems(clssExpired) {
 			}
 		}
 	});
+
 	// TODO: move expired list items to the back of the list, then redo layouts on any lists controlled by masonry JS.
 }
 
@@ -134,6 +190,7 @@ function initFacultyEmailAutoEntry(slctrSelectBox, slctrHiddenFields) {
 	var $nameInputBox;
 	var selectionMade;
 	var fieldsToFill;
+
 	// TODO: Update for Summer 2017
 	$(slctrSelectBox).each(function () {
 		$selectField = $(this);
