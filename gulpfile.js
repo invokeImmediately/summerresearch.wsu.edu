907c0c50ac32ed90b12ed4563513790744591c5c
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2020-04-13 22:22:33 -0700

    Refactor automation tasks for building CSS and JS files
    Refactor the file to utilize node module in WSU-UE---JS that centralizes code common to all gulp-mediated, DAESA-wide CSS and JS build processes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,118 @@
+/*!*************************************************************************************************
+ * gulpfile.js
+ * -------------------------------------------------------------------------------------------------
+ * SUMMARY: Gulp automation task definition file for setting up tasks that build CSS and JS
+ *   files for use on the WSUWP-based website of the WSU Summer Research Program.
+ *
+ * DESCRIPTION: This gulp automation task definition file is designed for use on the following
+ *   project that is maintained on GitHub:
+ *   https://github.com/invokeImmediately/summerresearch.wsu.edu
+ *
+ * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ *
+ * LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.
+ *
+ *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ *   without fee is hereby granted, provided that the above copyright notice and this permission
+ *   notice appear in all copies.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ *   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ *   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ *   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *   PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// TABLE OF CONTENTS
+// -----------------
+// §1: Gulp task dependencies..................................................................43
+// §2: Specificiation of build settings .......................................................48
+//   §2.1: getCssBuildSettings()...............................................................51
+//   §2.2: getJsBuildSettings()...............................................................103
+// §3: Entry point: Set up of build taks......................................................135
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+( function() {
+
 'use strict';
 
-/* -------------------------------------------------------------------------------------------------
-** Variable Declarations
-*/
 
-// Gulp task dependencies
-var cleanCss = require( 'gulp-clean-css' );
-var concat = require( 'gulp-concat' );
-var extName = require( 'gulp-extname' );
-var gcmq = require( 'gulp-group-css-media-queries' );
-var gulp = require( 'gulp' );
-var insert = require( 'gulp-insert' );
-var insertLines = require( 'gulp-insert-lines' );
-var lessc = require( 'gulp-less' );
-var replace = require( 'gulp-replace' );
-var uglifyJs = require( 'gulp-uglify' );
-var pump = require( 'pump' );
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §1: Gulp task dependencies
 
-/* -------------------------------------------------------------------------------------------------
-** Function declarations
-*/
+var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §2: Specificiation of build settings
+
+////////
+// §2.1: getCssBuildSettings()
+
+/**
+ * Get the settings for a gulp-mediated custom CSS build from Less source files.
+ *
+ * @return {object} - Instance of gulpBuilder.CssBuildSettings.
+ */
 function getCssBuildSettings() {
-	return {
-		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
-		dependenciesPath: './WSU-UE---CSS/',
-		destFolder: './CSS/',
-		fontImportStr: '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:300|Robot\
-o+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n',
-		insertingMediaQuerySectionHeader: {
-			'before': /^@media/,
-			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
-════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
-################################################################################# ║\r\n*   ╚═══════\
-═══════════════════════════════════════════════════════════════════════════════════════════════════\
-═════════════╝\r\n*/',
-			'stopAfterFirstMatch': true
-		},
-		minCssFileExtension: '.min.css',
-		minCssFileHeaderStr: '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Pleas\
-e see [https://github.com/invokeImmediately/summerresearch.wsu.edu] for a repository of source code\
-. */\r\n',
-		sourceFile: './CSS/summerresearch-custom.less'
+	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
+	var dependenciesPath = './WSU-UE---CSS/';
+	var destFolder = './CSS/';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i' +
+		',400,400i,600,600i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700' +
+		'i|Roboto+Mono:400,400i,700,700i&display=swap\');\r\n';
+	var insertingMediaQuerySectionHeader = {
+		'before': /^@media/,
+		'lineBefore': '/*! ======================================================================' +
+			'==========================\r\n*** Media queries section\r\n*** =====================' +
+			'===========================================================================\r\n*** S' +
+			'UMMARY: Media queries built from precompiled CSS written in the Less language extens' +
+			'ion of\r\n***   CSS. Queries in this section are a combination of those designed for' +
+			' use on all websites\r\n***   managed by WSU DAESA and those intended specifically f' +
+			'or use on the Summer Research Program\r\n***   website.\r\n***\r\n*** DESCRIPTION: F' +
+			'ully documented, precompiled source code from which this section of the custom\r\n**' +
+			'*   stylesheet was built is developed and maintained on the following two GitHub pro' +
+			'jects:\r\n***     https://github.com/invokeImmediately/WSU-UE---CSS/\r\n***     http' +
+			's://github.com/invokeImmediately/summerresearch.wsu.edu/\r\n***\r\n*** AUTHOR: Danie' +
+			'l Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)\r\n***\r\n*** ' +
+			'LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.\r\n***\r\n***   Permission to use' +
+			', copy, modify, and/or distribute this software for any purpose with or\r\n***   wit' +
+			'hout fee is hereby granted, provided that the above copyright notice and this permis' +
+			'sion\r\n***   notice appear in all copies.\r\n***\r\n***   THE SOFTWARE IS PROVIDED ' +
+			'"AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO\r\n***   THIS SO' +
+			'FTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT' +
+			'\r\n***   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONS' +
+			'EQUENTIAL DAMAGES OR\r\n***   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DAT' +
+			'A OR PROFITS, WHETHER IN AN ACTION OF\r\n***   CONTRACT, NEGLIGENCE OR OTHER TORTIOU' +
+			'S ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***   OR PERFORMANCE OF TH' +
+			'IS SOFTWARE.\r\n*** ================================================================' +
+			'================================\r\n**/',
+		'stopAfterFirstMatch': true
 	};
+	var minCssFileExtension = '.min.css';
+	var minCssFileHeaderStr = '';
+	var sourceFile = './CSS/summerresearch-custom.less';
+
+	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
+		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
+		minCssFileHeaderStr, sourceFile);
 }
 
+////////
+// §2.2: getJsBuildSettings()
+
+/**
+ * Get the settings for a gulp-mediated custom JS build.
+ *
+ * @return {object} - Simple collection of settings for JS builds.
+ */
 function getJsBuildSettings() {
 	return {
 		buildDependenciesList: [
 			'./WSU-UE---JS/jQuery.oue-custom.js',
-			'./WSU-UE---JS/jQuery.animatedCalendar.js',
 			'./WSU-UE---JS/jQuery.autoScrollingImages.js',
 			'./WSU-UE---JS/jQuery.cookieObjs.js',
-			'./WSU-UE---JS/jQuery.cycle2.js',
 			'./WSU-UE---JS/jQuery.forms.js',
 			'../jQuery.AreYouSure/jquery.are-you-sure.js',
 			'./WSU-UE---JS/jQuery.are-you-sure.js',
@@ -60,8 +120,6 @@ function getJsBuildSettings() {
 			'../qTip2/dist/jquery.qtip.min.js',
 			'./WSU-UE---JS/jQuery.qTip.js',
 			'./WSU-UE---JS/jQuery.textResize.js',
-			'../imagesloaded/imagesloaded.pkgd.min.js',
-			'../masonry/dist/masonry.pkgd.min.js',
 			'./WSU-UE---JS/jQuery.masonry-custom.js',
 			'./JS/sumres-custom.js'
 		],
@@ -69,64 +127,14 @@ function getJsBuildSettings() {
 		compiledJsFileName: 'sumres-build.js',
 		destFolder: './JS/',
 		minJsFileExtension: '.min.js',
-		replaceCallback: fixFileHeaderComments
+		replaceCallback: gulpBuilder.fixFileHeaderComments
 	};
 }
 
-function fixFileHeaderComments ( match, p1, offset, string ) {
-	var replacementStr = match;
-	if ( offset == 0 ) {
-		replacementStr = '/*!';
-	}
-	return replacementStr;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §3: Entry point: Set up of build taks
 
-function setUpCssBuildTask( settings ) {
-	gulp.task( 'buildMinCss', function ( callBack ) {
-		pump( [
-				gulp.src( settings.sourceFile ),
-				lessc( {
-					paths: [settings.dependenciesPath]
-				} ),
-				replace( settings.commentRemovalNeedle, '' ),
-				insert.prepend( settings.fontImportStr ),
-				insert.prepend( settings.minCssFileHeaderStr ),
-				gulp.dest( settings.destFolder ),
-				gcmq(),
-				insertLines( settings.insertingMediaQuerySectionHeader ),
-				cleanCss(),
-				extName( settings.minCssFileExtension ),
-				gulp.dest( settings.destFolder )
-			],
-			callBack
-		);
-	} );
-}
+gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
+gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
 
-function setUpJsBuildTask( settings ) {
-	gulp.task( 'buildMinJs', function ( callBack ) {
-		pump( [
-				gulp.src( settings.buildDependenciesList ),
-				replace( settings.commentNeedle, settings.replaceCallback ),
-				concat( settings.compiledJsFileName ),
-				gulp.dest( settings.destFolder ),
-				uglifyJs( {
-					output: {
-						comments: /^!/
-					},
-					toplevel: true,
-				} ),
-				extName( settings.minJsFileExtension ),
-				gulp.dest( settings.destFolder )
-			],
-			callBack
-		);
-	} );
-}
-
-/* -------------------------------------------------------------------------------------------------
-** Main execution sequence
-*/
-
-setUpCssBuildTask( getCssBuildSettings() );
-setUpJsBuildTask( getJsBuildSettings() );
+} )();
