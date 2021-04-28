d6e21eecb1bdb5d2d1660f1930202aff9a4dd847
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2021-04-28 15:01:35 -0700

    Production: Update CSS build settings
    Update CSS build settings in gulpfile.js to reflect the latest changes to how CSS builds are completed through gulp task automation via WSU-DAESA-JS/gulpCssJsBuilder.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,26 @@
 /*!*************************************************************************************************
- * gulpfile.js
- * -------------------------------------------------------------------------------------------------
- * SUMMARY: Gulp automation task definition file for setting up tasks that build CSS and JS
- *   files for use on the WSUWP-based website of the WSU Summer Research Program.
+ * █▀▀▀ █  █ █    █▀▀▄ █▀▀▀ ▀█▀ █    █▀▀▀      █ ▄▀▀▀
+ * █ ▀▄ █  █ █  ▄ █▄▄▀ █▀▀▀  █  █  ▄ █▀▀    ▄  █ ▀▀▀█
+ * ▀▀▀▀  ▀▀  ▀▀▀  █    ▀    ▀▀▀ ▀▀▀  ▀▀▀▀ ▀ ▀▄▄█ ▀▀▀ 
  *
- * DESCRIPTION: This gulp automation task definition file is designed for use on the following
- *   project that is maintained on GitHub:
- *   https://github.com/invokeImmediately/summerresearch.wsu.edu
+ * Gulp automation task definition file for setting up tasks that build CSS and JS files for use on
+ *   the WSUWP website of the WSU Summer Research Program.
  *
- * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- *
- * LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.
- *
- *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
- *   without fee is hereby granted, provided that the above copyright notice and this permission
- *   notice appear in all copies.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
- *   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
- *   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
- *   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
- *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- *   PERFORMANCE OF THIS SOFTWARE.
+ * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ * @link https://github.com/invokeImmediately/summerresearch.wsu.edu/blob/master/gulpfile.js
+ * @license MIT - Copyright (c) 2021 Washington State University
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *     and associated documentation files (the “Software”), to deal in the Software without
+ *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ *     distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+ *     the Software is furnished to do so, subject to the following conditions:
+ *   The above copyright notice and this permission notice shall be included in all copies or
+ *     substantial portions of the Software.
+ *   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ *     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **************************************************************************************************/
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -42,7 +41,7 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §1: Gulp task dependencies
 
-var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
+var gulpBuilder = require( './WSU-DAESA-JS/gulpCssJsBuilder.js' );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // §2: Specificiation of build settings
@@ -56,47 +55,72 @@ var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
  * @return {object} - Instance of gulpBuilder.CssBuildSettings.
  */
 function getCssBuildSettings() {
-	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
-	var dependenciesPath = './WSU-UE---CSS/';
-	var destFolder = './CSS/';
-	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i' +
-		',400,400i,600,600i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700' +
-		'i|Roboto+Mono:400,400i,700,700i&display=swap\');\r\n';
-	var insertingMediaQuerySectionHeader = {
-		'before': /^@media/,
-		'lineBefore': '/*! ======================================================================' +
-			'==========================\r\n*** Media queries section\r\n*** =====================' +
-			'===========================================================================\r\n*** S' +
-			'UMMARY: Media queries built from precompiled CSS written in the Less language extens' +
-			'ion of\r\n***   CSS. Queries in this section are a combination of those designed for' +
-			' use on all websites\r\n***   managed by WSU DAESA and those intended specifically f' +
-			'or use on the Summer Research Program\r\n***   website.\r\n***\r\n*** DESCRIPTION: F' +
-			'ully documented, precompiled source code from which this section of the custom\r\n**' +
-			'*   stylesheet was built is developed and maintained on the following two GitHub pro' +
-			'jects:\r\n***     https://github.com/invokeImmediately/WSU-UE---CSS/\r\n***     http' +
-			's://github.com/invokeImmediately/summerresearch.wsu.edu/\r\n***\r\n*** AUTHOR: Danie' +
-			'l Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)\r\n***\r\n*** ' +
-			'LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.\r\n***\r\n***   Permission to use' +
-			', copy, modify, and/or distribute this software for any purpose with or\r\n***   wit' +
-			'hout fee is hereby granted, provided that the above copyright notice and this permis' +
-			'sion\r\n***   notice appear in all copies.\r\n***\r\n***   THE SOFTWARE IS PROVIDED ' +
-			'"AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO\r\n***   THIS SO' +
-			'FTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT' +
-			'\r\n***   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONS' +
-			'EQUENTIAL DAMAGES OR\r\n***   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DAT' +
-			'A OR PROFITS, WHETHER IN AN ACTION OF\r\n***   CONTRACT, NEGLIGENCE OR OTHER TORTIOU' +
-			'S ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE\r\n***   OR PERFORMANCE OF TH' +
-			'IS SOFTWARE.\r\n*** ================================================================' +
-			'================================\r\n**/',
-		'stopAfterFirstMatch': true
-	};
-	var minCssFileExtension = '.min.css';
-	var minCssFileHeaderStr = '';
-	var sourceFile = './CSS/summerresearch-custom.less';
-
-	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
-		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
-		minCssFileHeaderStr, sourceFile);
+	return new gulpBuilder.CssBuildSettings( {
+		commentRemovalNeedle: /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm,
+		dependenciesPath: './WSU-DAESA-CSS/',
+		destFolder: './CSS/',
+		fontImportStr: '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,' +
+			'400i,600,600i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700i|Robot' +
+			'o+Mono:400,400i,700,700i&display=swap\');\r\n',
+		insertingMediaQuerySectionHeader: {
+			'before': /^@media/,
+				lineBefore: '/*! ========================================================================' +
+						'========================\r\n' +
+					'*** ▐▀▄▀▌█▀▀▀ █▀▀▄ ▀█▀ ▄▀▀▄   ▄▀▀▄ █  █ █▀▀▀ █▀▀▄ ▀█▀ █▀▀▀ ▄▀▀▀   ▄▀▀▀ █▀▀▀ ▄▀▀▀▐▀█▀▌' +
+						'▀█▀ ▄▀▀▄ ▐▀▀▄\r\n' +
+					'*** █ ▀ ▌█▀▀  █  █  █  █▄▄█   █  █ █  █ █▀▀  █▄▄▀  █  █▀▀  ▀▀▀█   ▀▀▀█ █▀▀  █     █  ' +
+						' █  █  █ █  ▐\r\n' +
+					'*** █   ▀▀▀▀▀ ▀▀▀  ▀▀▀ █  ▀    ▀█▄  ▀▀  ▀▀▀▀ ▀  ▀▄▀▀▀ ▀▀▀▀ ▀▀▀    ▀▀▀  ▀▀▀▀  ▀▀▀  █  ' +
+						'▀▀▀  ▀▀  ▀  ▐\r\n' +
+					'*** ==================================================================================' +
+						'==============\r\n' +
+					'*** Media queries built from precompiled CSS written in the Less language extension' +
+					  ' of CSS. Queries\r\n' +
+					'***   in this section are a combination of those designed for use on DAESA websites' +
+						' and those\r\n' +
+					'***   intended specifically for use on the Summer Research Program website.\r\n' +
+					'***\r\n' +
+					'*** Fully documented, precompiled source code from which this section of stylesheet' +
+						' was developed\r\n' +
+					'***   is maintained on the following two GitHub projects:\r\n' +
+					'***   - https://github.com/invokeImmediately/WSU-DAESA-CSS/\r\n' +
+					'***   - https://github.com/invokeImmediately/summerresearch.wsu.edu/\r\n' +
+					'***\r\n' +
+					'*** @author Daniel Rieck [daniel.rieck@wsu.edu]' +
+						' (https://github.com/invokeImmediately)\r\n' +
+					'*** @license: MIT - Copyright (c) 2021 Washington State University\r\n' +
+					'***   Permission is hereby granted, free of charge, to any person obtaining a copy of' +
+						' this software\r\n' +
+					'***     and associated documentation files (the "Software"), to deal in the Software' +
+						' without\r\n' +
+					'***     restriction, including without limitation the rights to use, copy, modify,' +
+						' merge, publish,\r\n' +
+					'***     distribute, sublicense, and/or sell copies of the Software, and to permit' +
+						' persons to whom\r\n' +
+					'***     the Software is furnished to do so, subject to the following conditions:\r\n' +
+					'***   The above copyright notice and this permission notice shall be included in all' +
+						' copies or\r\n' +
+					'***     substantial portions of the Software.\r\n' +
+					'***   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR' +
+						' IMPLIED, INCLUDING\r\n' +
+					'***     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR' +
+						' PURPOSE AND\r\n' +
+					'***     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE' +
+						' FOR ANY\r\n' +
+					'***     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR' +
+						' OTHERWISE,\r\n' +
+					'***     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER' +
+						' DEALINGS IN THE\r\n' +
+					'***     SOFTWARE.\r\n' +
+					'*** ==================================================================================' +
+						'==============\r\n' +
+					'**/',
+			'stopAfterFirstMatch': true
+		},
+		minCssFileExtension: '.min.css',
+		minCssFileHeaderStr: '',
+		sourceFile: './CSS/summerresearch-custom.less'
+	} );
 }
 
 ////////
